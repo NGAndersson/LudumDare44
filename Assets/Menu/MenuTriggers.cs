d6ce024bafe22854ccee970f260d25e790be7069
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2019-04-26 22:40:45 +0200

    Basic menu

---
+++ Assets/Menu/MenuTriggers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTriggers : MonoBehaviour
+{
+    public void Play()
+    {
+        StartCoroutine(LoadGameScene());
+    }
+
+    public void Options()
+    {
+        print("Options code goes here.");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadGameScene()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("SampleScene");
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
