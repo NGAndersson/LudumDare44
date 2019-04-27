fc929ccc560d272f7e7b0d3733d75b98c5170c77
Simon Hyltegard <simon.home@hotmail.com>
2019-04-27 20:45:35 +0200

    Meck

---
+++ Assets/3DModels/ScoreGivers/Stretcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stretcher : MonoBehaviour
+{
+    public Animator animController;
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, player.position) < 10f)
+        {
+            animController.SetBool("Stretch", true);
+        }
+        else
+        {
+            animController.SetBool("Stretch", false);
+        }
+    }
+}
