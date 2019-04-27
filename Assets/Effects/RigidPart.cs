a689a9440d211652671d91d6c3de8ef8a03eb242
Simon Hyltegard <simon.home@hotmail.com>
2019-04-27 13:50:49 +0200

    Added gore stuff

---
+++ Assets/Effects/RigidPart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidPart : MonoBehaviour
+{
+    public Rigidbody rBody;
+
+    public void Spawn(Vector3 direction, float force)
+    {
+        this.gameObject.SetActive(true);
+
+        force = force * Random.Range(0.7f, 1.3f);
+
+        float posOffset = Random.Range(-1, 1);
+        this.transform.localPosition += new Vector3(posOffset, 0, posOffset);
+
+        Vector3 randomVector = new Vector3(Random.value, Random.value, Random.value).normalized;
+        this.transform.localRotation = Quaternion.Euler(randomVector);
+
+        float scaleMult = Random.Range(0.6f, 2f);
+        this.transform.localScale = this.transform.localScale * scaleMult;
+
+        this.rBody.AddForce((direction + (randomVector * 0.5f)) * force, ForceMode.Impulse);
+        this.rBody.AddTorque(randomVector * force, ForceMode.Impulse);
+
+        StartCoroutine(LifeTime());
+    }
+
+    private IEnumerator LifeTime()
+    {
+        yield return new WaitForSeconds(3);
+        rBody.isKinematic = true;
+        yield return new WaitForSeconds(10);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Effects/SpawnEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEffect : MonoBehaviour
+{
+    public Vector2 nrSpawnObjectVariation = new Vector2(2, 4);
+    public RigidPart[] spawnObjectPrefabs;
+
+
+    private List<RigidPart> spawnObjects = new List<RigidPart>();
+    private ParticleSystem ps;
+
+    private void Awake()
+    {
+        int nrToSpawn = Random.Range((int)nrSpawnObjectVariation.x, (int)nrSpawnObjectVariation.y);
+
+        for (int i = 0; i < nrToSpawn; i++)
+        {
+            RigidPart toSpawn = spawnObjectPrefabs[Random.Range(0, spawnObjectPrefabs.Length - 1)];
+            GameObject go = Instantiate(toSpawn.gameObject);
+
+            go.SetActive(false);
+            spawnObjects.Add(go.GetComponent<RigidPart>());
+        }
+    }
+
+    public void Spawn(Vector3 direction, float force)
+    {
+        for (int i = 0; i < spawnObjects.Count; i++)
+        {
+            spawnObjects[i].transform.position = this.transform.position;
+            spawnObjects[i].Spawn(direction, force);
+        }
+
+        // Direct the particles, assumes that the particlesystem is pointing forward.
+        this.transform.forward = direction;
+
+        StartCoroutine(LifeTime());
+    }
+
+    private IEnumerator LifeTime()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(this.gameObject);
+    }
+}
