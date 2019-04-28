2061cda10b8f9ba1c7096dd340a974ed230c799b
Simon Hyltegard <simon.home@hotmail.com>
2019-04-28 09:25:11 +0200

    Added samboni

---
+++ Assets/Enemy/Samboni.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Samboni : MonoBehaviour
+{
+    public Transform[] checkPoints;
+
+    private int currIndex = 0;
+
+    private float startYPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startYPos = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Rigidbody>().velocity = this.transform.forward * 10;
+        this.transform.position = new Vector3(this.transform.position.x, startYPos, this.transform.position.z);
+        //this.transform.Translate(-this.transform.right * Time.deltaTime * 4f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Rink")
+        {
+            //Vector3 dir = checkPoints[currIndex].position;
+            this.transform.LookAt(checkPoints[currIndex].position);
+
+            currIndex++;
+
+            if (currIndex >= checkPoints.Length)
+            {
+                currIndex = 0;
+            }
+        }
+    }
+}
